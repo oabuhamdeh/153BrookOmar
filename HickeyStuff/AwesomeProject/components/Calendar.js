a95f5c9b117abb0700e93ebc13c2304bb54ca1c0
Brooke Schwartz <brookedschwartz@gmail.com>
2023-07-04 17:24:34 -0500

    brooke

--- HickeyStuff/AwesomeProject/components/Calendar.js
+++ HickeyStuff/AwesomeProject/components/Calendar.js
@@ -1,27 +1,87 @@
-import React, {useState} from 'react';
-import {Calendar, LocaleConfig, selectedDotColor, Periodmarking, Agenda} from 'react-native-calendars';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
+import { Agenda } from 'react-native-calendars';
+import { Card } from 'react-native-paper';
+
+const timeToString = (time) => {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+}
 
 const App = () => {
-  const [selected, setSelected] = useState('');
+    const [items, setItems] = React.useState({});
 
-  return (
-    <Calendar theme={{
-        backgroundColor: '#ffffff',
-        calendarBackground: '#ffffff',
-        textSectionTitleColor: '#b6c1cd',
-        selectedDayBackgroundColor: '#00adf5',
-        selectedDayTextColor: '#ffffff',
-        todayTextColor: '#00adf5',
-        dayTextColor: '#2d4150',
-        textDisabledColor: '#d9e1e8',}}
-      onDayPress={day => {
-        setSelected(day.dateString);
-      }}
-      markedDates={{
-        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-      }}
-    />
-  );
-};
+    const loadItems = (day) => {
+
+        setTimeout(() => {
+            for (let i = -15; i < 85; i++) {
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+
+                const strTime = timeToString(time);
+
+                if (!items[strTime]) {
+                    items[strTime] = [];
+
+                    const numItems = Math.floor(Math.random() * 3 + 1);
+                    for (let j = 1; j < numItems; j++) {
+                        items[strTime].push({
+                            name: 'Item for ' + strTime + ' #' + j,
+                            height: Math.max(10, Math.floor(Math.random() * 150)),
+                            day: strTime
+                        });
+                    }
+                }
+            }
+            const newItems = {};
+            Object.keys(items).forEach(key => {
+                newItems[key] = items[key];
+            });
+            setItems(newItems);
+        }, 1000);
+    }
+
+    const renderItem = (item) => {
+        return (
+            <TouchableOpacity style={styles.item}>
+                <Card>
+                    <Card.Content>
+                        <View>
+                            <Text>{item.name}</Text>
+                        </View>
+                    </Card.Content>
+                </Card>
+            </TouchableOpacity>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Agenda
+                items={items}
+                loadItemsForMonth={loadItems}
+                selected={'2023-07-07'}
+                refreshControl={null}
+                showClosingKnob={true}
+                refreshing={false}
+                renderItem={renderItem}
+                enableSwipeMonths
+            />
+            <StatusBar />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    item: {
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17
+    },
+});
 
 export default App;
